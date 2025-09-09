74980223d270b596fcdd760912f182e4bc2e00d6
VanezaGaytan <vaneza.cgaytan@gmail.com>
2025-09-09 00:34:16 -0400

    Version finale: nom, date, somme et Fibonacci

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import datetime
+from utils import fibonacci 
+
 if __name__ == "__main__":
     name = input("Quel est ton nom ? ")
     print(f"Bienvenue {name} dans le laboratoire Git + Python !")
@@ -6,4 +8,6 @@ a = 5
 b = 7
 print(f"La somme de {a} et {b} est {a+b}")
 
-print(f"Bienvenue {name}"- date branch)
+print(f"Bienvenue {name}, aujourd'hui nous sommes le {datetime.date.today()}")
+
+print("Suite de Fibonacci (10 premiers termes):", fibonacci(10))
